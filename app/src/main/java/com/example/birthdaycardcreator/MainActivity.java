15c35ef568d167c9a547969924a586bf3c37675d
Aadarsh Agarwal <aadarshagarwal2229@gmail.com>
2020-09-18 20:40:52 +0530

    UPDATING APP

--- app/src/main/java/com/example/birthdaycardcreator/MainActivity.java
+++ app/src/main/java/com/example/birthdaycardcreator/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.birthdaycardcreator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText nameInput;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameInput = findViewById(R.id.nameInput);
+    }
+
+    public void createBirthdayCard(View view)
+    {
+        Toast.makeText(this, "Name is "+ nameInput, Toast.LENGTH_SHORT).show();
     }
 }
